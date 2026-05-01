0f3e07bea7273e16b3ed61c5d241c9d916c0dbe6
Søren Døssing <netcom@sauber.net>
2026-05-02 00:29:04 +0200

    Outline process for passenger creation

---
+++ src/create-passenger.ts
@@ -0,0 +1,19 @@
+import { Passenger } from "./passenger.ts";
+import type { State } from "./state.ts";
+import type { Station } from "./station.ts";
+
+/** Create passenger at station */
+export function createPassenger(
+  game: State,
+  station: Station,
+): Passenger | Error {
+  // TODO:
+  // Destinations reachable from station
+  // const destinations = findDestinations(state, station);
+  // Pick a destination (random weighted by station size and distance)
+  // Create passenger
+  // Add passenger to station
+
+  const destination = [...game.stations][0];
+  return new Passenger(station, destination);
+}
